2349140d247906de2c1fb7d2b3f50dbe2f4a8df0
Roni Choudhury <roni.choudhury@kitware.com>
2016-11-03 23:04:17 -0400

    Add "extract title" function and call it during processing

--- app/startrek/parsechakoteya.hs
+++ app/startrek/parsechakoteya.hs
@@ -61,6 +61,11 @@ parseRawLine s = parse parser s s
 -- Parse HTML text.
 parseHTML = readString [withParseHTML yes, withWarnings no]
 
+-- Get the episode title from the HTML.
+extractScriptTitle html = do
+    titleText <- head <$> (runX $ html >>> css "title" //> getText)
+    return $ strip . unnewline . intercalate "-" $ (tail $ splitOn "-" titleText)
+
 -- Extract all appropriate text nodes comprising script.
 extractScriptText html = runX $ html >>> css "table font" //> getText
 
@@ -145,6 +150,7 @@ main = do
 
     -- Open the file, read its contents, and parse out the script lines from the
     -- HTML.
+    title <- extractScriptTitle . parseHTML $ text
     script <- readHTMLScript text
 
     -- Extract the appropriate tags from the text.
