800fba2b7c16ee1519540beb5b5a7f98e71d8127
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-22 11:58:43 -0400

    Add a list grouping function

--- src/Data/Bechdel/Util.hs
+++ src/Data/Bechdel/Util.hs
@@ -6,3 +6,14 @@ unnewline = map nlToSpace
     where nlToSpace :: Char -> Char
           nlToSpace '\n' = ' '
           nlToSpace c = c
+
+-- Split a list into sublists by a predicate.
+splitGroups :: (a -> Bool) -> [a] -> [[a]]
+splitGroups _ [] = []
+splitGroups f xs = splitGroups' [] [] f xs
+  where
+    splitGroups' :: [[a]] -> [a] -> (a -> Bool) -> [a] -> [[a]]
+    splitGroups' accum current _ [] = reverse $ current:accum
+    splitGroups' accum current f (x:xs) = if f x
+        then splitGroups' ((reverse current):accum) [x] f xs
+        else splitGroups' accum (x:current) f xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,6 @@
+import Data.Bechdel.Util as U
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  print $ U.splitGroups (== 3) [3, 1, 4, 5, 2, 3, 1, 3, 3, 6, 8, 3]
+  print $ U.splitGroups (== 3) [2, 1, 4, 5, 2, 3, 1, 3, 3, 6, 8, 3]
